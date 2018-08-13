d0afa7755dfe16ab04c96377fe8dacae07c0898b
Andreas Schultz <aschultz@tpip.net>
2018-08-13 15:58:10 +0200

    [DEBUG] configuration

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/sh -x
 
-if [[ $EUID > 0 ]]; then
+if [ $(id -u) -ne 0 ]; then
     exec sudo -E "$0" "$@"
 fi
 
@@ -12,11 +12,11 @@ ARGS="-c $base/startup.conf"
 USAGE="Usage: run.sh [ debug ]
        debug:	executes vpp under gdb"
 
-if [ -z "$1" ]; then
+if test -z "$1"; then
     $APP $ARGS
-elif [ "$1" == "debug" ]; then
-     GDB_EX="-ex 'set print pretty on' "
-     gdb $GDB_EX --args $APP $ARGS
+elif test "$1" = "debug"; then
+    shift
+    gdb -ex 'set print pretty on' -ex 'run' --args $APP $ARGS $@
 else
-	echo "$USAGE"
+    echo "$USAGE"
 fi
