497abd7e331d4cd6233847a53b4f697e82882226
Andreas Schultz <aschultz@tpip.net>
2018-12-18 17:03:48 +0000

    [DEBUG] run with debug or release build

--- run.sh
+++ run.sh
@@ -7,10 +7,25 @@ fi
 base=$(dirname $0)
 
 APP="$base/build-root/install-vpp_debug-native/vpp/bin/vpp"
-ARGS="-c $base/startup.conf"
+ARGS="-c $base/startup_debug.conf"
 
-USAGE="Usage: run.sh [ debug ]
+USAGE="Usage: run.sh [-r] [ debug ]
        debug:	executes vpp under gdb"
+
+while getopts ":r" opt; do
+    case $opt in
+	r)
+	    APP="$base/build-root/install-vpp-native/vpp/bin/vpp"
+	    ARGS="-c $base/startup.conf"
+	    ;;
+	\?)
+	    echo "Invalid option: -$OPTARG\n" >&2
+	    echo "$USAGE" >&2
+	    exit 1
+	    ;;
+    esac
+done
+shift $((OPTIND-1))
 
 if test -z "$1"; then
     $APP $ARGS
@@ -18,5 +33,6 @@ elif test "$1" = "debug"; then
     shift
     gdb -ex 'set print pretty on' -ex 'run' --args $APP $ARGS $@
 else
-    echo "$USAGE"
+    echo "$USAGE" >&2
+    exit 1
 fi
